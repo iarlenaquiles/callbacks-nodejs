321c33f6a7f2beef7f293e0e60f24581a6e8651b
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-01-29 23:48:11 -0300

    Refatorando as callbacks para promisses

--- index.js
+++ index.js
@@ -4,59 +4,78 @@
 2 - obter o endereco do usuario pelo id
 */
 
-function obterUsuario(callback) {
-    setTimeout(function () {
-        return callback(null, {
-            id: 1,
-            nome: 'Shrek',
-            dataNascimento: new Date()
-        })
-    }, 1000)
-}
-
-function obterTelefone(idUsuario, callback) {
-    setTimeout(function () {
-        return callback(null, {
-            telefone: '11111111',
-            ddd: '34'
-        })
-    }, 2000)
-}
-
-function obterEndereco(idUsuario, callback) {
-    setTimeout(function () {
-        return callback(null, {
-            rua: 'teste',
-            bairro: 'centro'
-        })
-    }, 2000)
-}
-
-obterUsuario(function resolverUsuario(error, usuario) {
-    if (error) {
-        console.error("Deu ruim em usuario", error)
-        return;
-    }
-
-    console.log(usuario);
-
-    obterTelefone(usuario.id, function resolverTelefone(errorTel, telefone) {
-        if (errorTel) {
-            console.error("Deu ruim em telefone", errorTel)
-            return;
-        }
-
-        console.log(telefone);
-
-        obterEndereco(usuario.id, function resolverEndereco(erroEnd, endereco) {
-            if (erroEnd) {
-                console.error("Deu ruim em telefone", errorTel)
-                return;
-            }
-            console.log(endereco);
-        })
+function obterUsuario() {
+    return new Promise(function resolvePromisse(resolve, reject) {
+        setTimeout(function () {
+            return resolve({
+                id: 1,
+                nome: 'Shrek',
+                dataNascimento: new Date()
+            })
+        }, 1000)
     })
+}
+
+function obterTelefone(idUsuario) {
+    return new Promise(function resolvePromisse(resolve, reject) {
+        setTimeout(function () {
+            return resolve({
+                telefone: '11111111',
+                ddd: '34'
+            })
+        }, 2000)
+    })
+}
+
+function obterEndereco(idUsuario) {
+    return new Promise(function resolvePromisse(resolve, reject) {
+        setTimeout(function () {
+            return resolve({
+                rua: 'teste',
+                bairro: 'centro'
+            })
+        }, 2000)
+    })
+}
+
+const usuario = obterUsuario()
+usuario.then(function (res) {
+    return obterTelefone(res.id).then(function resolveTelefone(result) {
+        return {
+            usuario: res,
+            telefone: result
+        }
+    })
+}).then(function (res) {
+    console.log('resultado', res)
+}).catch(function (error) {
+    console.error('Deu ruim no user', error)
 })
+// obterUsuario(function resolverUsuario(error, usuario) {
+//     if (error) {
+//         console.error("Deu ruim em usuario", error)
+//         return;
+//     }
+
+//     console.log(usuario);
+
+//     obterTelefone(usuario.id, function resolverTelefone(errorTel, telefone) {
+//         if (errorTel) {
+//             console.error("Deu ruim em telefone", errorTel)
+//             return;
+//         }
+
+//         console.log(telefone);
+
+//         obterEndereco(usuario.id, function resolverEndereco(erroEnd, endereco) {
+//             if (erroEnd) {
+//                 console.error("Deu ruim em telefone", errorTel)
+//                 return;
+//             }
+//             console.log(endereco);
+//         })
+//     })
+// })
 
 //const telefone = obterTelefone(usuario.id)
 //console.log('telefone', telefone)
